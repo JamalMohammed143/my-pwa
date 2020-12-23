bb4835f6e09507cbc075064cf70ee304bc3d2f84
Jamal Mohammed <jamalmohd28051994@gmail.com>
2020-12-23 11:11:04 +0530

    files modify

--- public/sw.js
+++ public/sw.js
@@ -1,12 +1,12 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", e => {
+self.addEventListener("activate", (e) => {
   return self.clients.claim();
 });
 
-self.addEventListener("fetch", e => {
+self.addEventListener("fetch", (e) => {
   // DevTools opening will trigger these o-i-c requests, which this SW can't handle.
   if (e.request.cache === "only-if-cached" && e.request.mode !== "same-origin")
     return;
